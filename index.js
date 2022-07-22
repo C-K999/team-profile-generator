bfa22d4ec38fd566375ac582b48cb15fe61d1434
C K <ctkoo@ucsc.edu>
2022-07-21 22:25:40 -0700

    made classes

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+// Team's basic info
+/*
+
+Refrences:
+- 13-Ins_Pass-Tests
+- 25-Ins_Multiple-Classes
+- 28-Stu_Mini-Project
+
+*/
