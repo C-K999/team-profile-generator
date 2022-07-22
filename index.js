0427d589e1ca13046d23f7260e23e672ec9bdec0
C K <ctkoo@ucsc.edu>
2022-07-22 02:16:39 -0700

    code redone

--- index.js
+++ index.js
@@ -1,15 +1,119 @@
+//modules and classes
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-// Team's basic info
-/*
+const memberList = [];
 
-Refrences:
-- 13-Ins_Pass-Tests
-- 25-Ins_Multiple-Classes
-- 28-Stu_Mini-Project
+const addManager = () =>{
+    return inquirer.prompt([
+    {
+        type:'input',
+        message:'Name of the manager?',
+        name: 'manageName'
+    },
+    {
+        type:'input',
+        message: "Manager's ID?",
+        name: 'manageId'
+    },
+    {
+        type:'input',
+        message:"Manager's email?",
+        name:'manageEmail'
+    },
+    {
+        type:'input',
+        message: "Manager's office number?",
+        name:'manageNumber'
+    }
+    ]).then((response)=>{
+        const  { manageName, manageId, manageEmail, manageNumber } = response;
+        const manager = new Manager (manageName, manageId, manageEmail, manageNumber); 
+        memberList.push(manager);
+        console.log(memberList);
+    })
+}
 
-*/
+const add_Employee = () =>{
+    return inquirer.prompt([
+        {
+            type:'input',
+            message:'Name of the employee?',
+            name: 'name'
+        },
+        {
+            type:'input',
+            message: "Employee's ID?",
+            name: 'id'
+        },
+        {
+            type:'input',
+            message:"Employee's email?",
+            name:'email'
+        },
+        {
+            type:'input',
+            message:"Employee's position?",
+            name:'position'
+        },
+        {
+            type:'input',
+            message: "Employee's github username?",
+            name:'github'
+        },
+        {
+            type:'input',
+            message: "Employee's school?",
+            name:'school'
+        },
+        {
+            type: 'confirm',
+            name: 'AddEmployee',
+            message: 'Would you like to add another member?',
+            default: false
+        }
+    ]).then((response)=>{
+        var { name, id, email, position, github, school, AddEmployee } = response;
+        var employee;
+
+        if(position.toString().toLowerCase()==='engineer'){
+            employee = new Engineer(name, id, email, github);
+        }
+        if(position.toString().toLowerCase()==='intern'){
+            employee = new Intern(name, id, email, school);
+        }
+        memberList.push(employee);
+        console.log(memberList);
+
+        if(AddEmployee){
+            return add_Employee(memberList);
+        }else{
+            return memberList;
+        }
+    })
+}
+
+const output = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Done! Your profile has been created! Please head over to the 'dist' folder!")
+        }
+    })
+};
+
+addManager()
+  .then(add_Employee)
+  .then(pageHTML => {
+    return output(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
